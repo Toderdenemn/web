4983a328ed92a9a0ab47b4875eda80160f9fb3ee
Evening <evening@admins-iMac-3.local>
2022-11-28 17:27:26 +0800

    lesson

---
+++ Lesson/main.js
@@ -0,0 +1,24 @@
+
+//  let points = 100
+//  const bonusPts = 40
+ 
+//  points = points + bonusPts
+//  console.log(points)
+
+ //score = 0
+
+
+ //score = 'message' //score buyu ur dung hucheer solij bolno//
+
+
+  //console deer score-g haruul gesen utga//
+
+
+
+ // let buyu var.n uurchilj bolldog utga zaana
+ // const bol uurchilj bolohgui utgiig zaana
+
+ let x = 986678
+ let y = 872364
+ let m = (y*2-y*1)/(x*2-x*1)
+ console.log(m)
