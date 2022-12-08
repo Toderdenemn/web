ef068edb5c9ceaddd4d18ff5a4a92081bb4715bf
Toderdene <72442586+Toderdenemn@users.noreply.github.com>
2022-12-08 16:26:14 +0800

    update

---
+++ Lesson/object.js
@@ -0,0 +1,22 @@
+// object ni neg huwisagch dotor olon medeelel oruulah bolomjtoi
+// object dotor davhar ocject uusgej bolno
+// object dotor function orj bolno
+
+// jishee:
+const bold = {
+    name: 'Bold',
+    age: 17,
+    isMarried: true,
+    mangar: 'tiim',
+    eruulMend: {
+        teneg: 'tiim',
+    }
+}
+console.log(bold)
+
+
+// object clone hiij bolno
+
+let bat = {...bold};
+
+console.log(bat)
