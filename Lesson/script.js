471fcb158c2da43cdb964e8fd7a063fbd79dc097
Toderdene <72442586+Toderdenemn@users.noreply.github.com>
2023-04-14 00:38:33 +0800

    new

--- Lesson/script.js
+++
@@ -1,7 +0,0 @@
-let password= "pass"
-let code 
-while(password != code){
-    code=prompt('buruu')}
-
-alert("ikon.mn сайтруу нэвтрэх үү?")
- location.href = "https://www.ikon.mn"
--- Lesson/ternary_operator.js
+++
@@ -1,4 +0,0 @@
-const message = "mangar"
-
-const html = `teneg teneg ${message} teneg`
-console.log(html)
